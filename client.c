48999aa9124ec3ca293f354a24681818a0df06a2
Conan <cwong7@stuy.edu>
2017-01-12 11:46:33 -0500

    files added

---
+++ client.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main(){
+  // Connecting to server [3 way handshake]
+  
+  // while loop
+
+  // clear terminal window
+  // reading data from server
+  // input request (rps)
+  
+  // write input to server
+  // read data (won rock paper scissors or nah?)
+
+  // change display to chopstics + output on who won
+  // give choice (fgets + write)  or wait (read)
+
+  // update display, then wait 2-3 seconds before loop
+
+}
+
+void display(int ohl, int ohr, int mhl, int mhr){
+}
