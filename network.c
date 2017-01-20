632b875c6fde0bca995ad76d216ae849836039b5
achencoms <achencoms@gmail.com>
2017-01-20 08:11:16 -0500

    Add files via upload

--- network.c
+++ network.c
@@ -9,65 +9,63 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#include "network.h"
-
-
-void errorhandle(int i){
-   if (i < 0){
-      printf("Error: %s", strerror(errno));
-   }
+void error_check( int i, char *s ) {
+  if ( i < 0 ) {
+    printf("%d\n", i);
+    printf("[%s] error %d: %s\n", s, errno, strerror(errno) );
+    exit(1);
+  }
 }
 
-int server_setup(){
-   int sd;
-   int i;
-
-   sd = socket( AF_INET, SOCK_STREAM, 0);
-   errorhandle(sd);
-
-   struct sockaddr_in sock;
-   sock.sin_family = AF_INET;
-   sock.sin_addr.s_addr = INADDR_ANY;
-   sock.sin_port = htons(2500);
-
-   i = bind(sd, (struct sockaddr *)&sock, sizeof(sock));
-   errorhandle(i);
-   
-   return sd;
-}
-
-int server_connect(int sd){
-   int connection, i;
-	
-   i = listen(sd , 1);
-   errorhandle(i);
-   
-   struct sockaddr_in sock1;
-   unsigned int sock1_len = sizeof(sock1);
-   connection = accept(sd, (struct sockaddr *)&sock1, &sock1_len);
-
-   errorhandle( connection );
+int server_setup() {
   
-   printf("[server] connected to %s\n", inet_ntoa( sock1.sin_addr ) );
+  int sd;
+  int i;
   
-   return connection;
+  sd = socket( AF_INET, SOCK_STREAM, 0 );
+  error_check( sd, "server socket" );
+  
+  struct sockaddr_in sock;
+  sock.sin_family = AF_INET;
+  sock.sin_addr.s_addr = INADDR_ANY;
+  sock.sin_port = htons(9001);
+  i = bind( sd, (struct sockaddr *)&sock, sizeof(sock) );
+  error_check( i, "server bind" );
+  
+  return sd;
 }
+
+int server_connect(int sd) {
+  int connection, i;
+
+  i = listen(sd, 1);
+  error_check( i, "server listen" );
+  
+  struct sockaddr_in sock1;
+  unsigned int sock1_len = sizeof(sock1);
+  connection = accept( sd, (struct sockaddr *)&sock1, &sock1_len );
+  error_check( connection, "server accept" );
+  
+  printf("[server] connected to %s\n", inet_ntoa( sock1.sin_addr ) );
+  
+  return connection;
+}
+
 
 int client_connect( char *host ) {
-   int sd, i;
+  int sd, i;
   
-   sd = socket( AF_INET, SOCK_STREAM, 0 );
-   errorhandle( sd );
+  sd = socket( AF_INET, SOCK_STREAM, 0 );
+  error_check( sd, "client socket" );
   
-   struct sockaddr_in sock;
-   sock.sin_family = AF_INET;
-   inet_aton( host, &(sock.sin_addr));
-   sock.sin_port = htons(2500);
+  struct sockaddr_in sock;
+  sock.sin_family = AF_INET;
+  inet_aton( host, &(sock.sin_addr));
+  sock.sin_port = htons(9001);
   
-   printf("[client] connecting to: %s\n", host );
-   i = connect( sd, (struct sockaddr *)&sock, sizeof(sock) );
-   errorhandle(i);
+  printf("[client] connecting to: %s\n", host );
+  i = connect( sd, (struct sockaddr *)&sock, sizeof(sock) );
+  error_check( i, "client connect");
   
-   return sd;
+  return sd;
 }
-
