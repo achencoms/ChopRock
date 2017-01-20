6778e011c54688ad4169d0e94d1713139a76be3a
achencoms <achencoms@gmail.com>
2017-01-20 04:55:38 -0500

    Add files via upload

--- server.c
+++ server.c
@@ -6,21 +6,50 @@
 
 #include "network.h"
 
+// need to add in the server taking in the requests of the user
+// i.e. Which hand would you like to use?, Which hand would you like to attack?
+// The loser is the one who has both hands = 0
+
 int main(){
    int sd;
- 
+   char buff[128];
+   char turn[] = "It is your turn to go\n";
+   char notturn[] = "It is the other player's turn, please wait...\n";
+
    sd = server_setup();
    while(1){
-  	int connection = server_connect( sd );
+  	int connection1 = server_connect( sd );
+	printf("Player 1 connected to the server! Waiting for a match...\n");
+        int connection2 = server_connect( sd );
+	printf("Player 2 connected to the server! Match ready!\n");
+	strcpy(buff, "You have found a match!\n");
+	write(connection1, buff, sizeof(buff));
+	write(connection2, buff, sizeof(buff));
 
 	int f = fork();
 	if(f == 0){
-   	 char buff[128];
- 	 while(read(connection, buff, sizeof(buff))){
-	  printf("Received message: %s", buff);
-   	  strncpy(buff, "Thanks for the foo", sizeof(buff));
-   	  write(connection, buff, sizeof(buff));
+  	 int choose = 0;
+	while(1){
+	//while(read(connection1, buff, sizeof(buff)) || read(connection2, buff, sizeof(buff))){
+         if(choose == 0){
+	  strcpy(buff,turn);
+	  write(connection1, buff, sizeof(buff));
+	  strcpy(buff,notturn);
+	  write(connection2, buff, sizeof(buff));
+ 	  read(connection1, buff, sizeof(buff));
+	  printf("Message from Player 1: %s", buff);
+	  choose = 1;
    	 }
+	 else{
+	  strcpy(buff,turn);
+	  write(connection2, buff, sizeof(buff));
+	  strcpy(buff,notturn);
+	  write(connection1, buff, sizeof(buff));
+	  read(connection2, buff, sizeof(buff));
+	  printf("Message from Player 2: %s", buff);
+	  choose = 0;
+	 }
+	}
 	 exit(0);
         }
    }
